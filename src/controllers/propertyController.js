3cb28ebaf083389148ce0e7b83e9cbd367fc03e5
Chairlessdev <jesen842@gmail.com>
2023-09-15 16:41:38 -0700

    ??

--- src/controllers/propertyController.js
+++ src/controllers/propertyController.js
@@ -1,24 +1,23 @@
-// controllers/propertyController.js
 const Property = require('../models/Property');
 
 exports.listProperties = async (req, res, next) => {
-  let query = {};
-  
+  let queryDB = {};
+
   // Manejar filtros
   if (req.query.propertyType) {
-    query.propertyType = req.query.propertyType;
+    queryDB.property_type = req.query.propertyType;
   }
-  
+
   if (req.query.roomType) {
-    query.roomType = req.query.roomType;
+    queryDB.room_type = req.query.roomType;
   }
-  
+
   if (req.query.bedrooms) {
-    query.bedrooms = req.query.bedrooms;
+    queryDB.bedrooms = req.query.bedrooms;
   }
 
   if (req.query.name) {
-    query.name = {
+    queryDB.name = {
       $regex: new RegExp(req.query.name, "i")  // i para insensible a mayúsculas y minúsculas
     };
   }
@@ -28,9 +27,9 @@ exports.listProperties = async (req, res, next) => {
   const limit = parseInt(req.query.limit, 10) || 10;
   const skip = (page - 1) * limit;
 
-  const total = await Property.countDocuments(query);
-  const properties = await Property.find(query).skip(skip).limit(limit);
-    
+  const total = await Property.countDocuments(queryDB);
+  const properties = await Property.find(queryDB).skip(skip).limit(limit);
+
   res.json({
     total,
     page,
